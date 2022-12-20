923b875b446d8dc1347304e6df5612e80648ad23
EkaterinaRomankova <catia.romankova@yandex.by>
2022-12-20 21:06:13 +0300

    k

--- cypress/e2e/first_test.cy.js
+++ cypress/e2e/first_test.cy.js
@@ -1,10 +1,10 @@
 /// <reference types="cypress" />
 
 describe('My first test', () => {
-    
+
     it ('check buttion "server"', () =>{
         cy.visit('https://example.cypress.io')
-        cy.get('a[href = "/cypress-api"]').contains("Server").click()
+        cy.get('a[href = "/cypress-api"]').contains("Serve").click()
         cy.url().should('include', '/cypress-api')
     })
 })
