bf7e478807fe0cf5dd0ae613cb1feb01b53b5e0c
EkaterinaRomankova <catia.romankova@yandex.by>
2023-01-02 21:20:13 +0300

    f

--- cypress/e2e/bookingListSpec.cy.js
+++ cypress/e2e/bookingListSpec.cy.js
@@ -40,7 +40,30 @@ describe('create booking page', () => {
         cy.wait(9000)
         bookingList.elements.getAllStatus().each(($el, i) => {
             expect(this.data.allStatuses).to.include($el.text())
-        })
-        
+        })    
+    })
+
+    xit('check route colom', function() {
+        registerPage.clickOnButtonLogin()
+        cy.logIn(this.data.email, this.data.password)
+        cy.wait(9000)
+        bookingList.clickOnBokinglistOfOrders()
+        bookingList.elements.getRouteSelect().select(this.data.routeWay1, {force: true})
+        cy.wait(9000)
+        bookingList.elements.getRouteColom().each(($el) => {
+            expect($el.text()).to.include(this.data.routeWayColom1)
+        })      
+    })
+
+    xit('check route colom', function() {
+        registerPage.clickOnButtonLogin()
+        cy.logIn(this.data.email, this.data.password)
+        cy.wait(9000)
+        bookingList.clickOnBokinglistOfOrders()
+        bookingList.elements.getRouteSelect().select(this.data.routeWay2, {force: true})
+        cy.wait(9000)
+        bookingList.elements.getRouteColom().each(($el) => {
+            expect(this.data.routeWayColom2).to.include($el.text())
+        })          
     })
 })    
--- cypress/pageObject/BookingListPage.js
+++ cypress/pageObject/BookingListPage.js
@@ -3,7 +3,9 @@ class BookingListPage{
         getBookingListOfOrders: () => cy.get('a[href="/orders/"]'),
         getStatusColom: () => cy.get('#others-filtres #filterStatus'),
         getAllStatus: () => cy.get('#data nobr'),
-        getCloseChosenStatus: () => cy.get('.select2-selection__choice__remove')
+        getCloseChosenStatus: () => cy.get('.select2-selection__choice__remove'),
+        getRouteSelect: () => cy.get('#others-filtres #filterRoute'),
+        getRouteColom: () => cy.get('tbody :nth-child(4)')
     }
 
     clickOnBokinglistOfOrders(){
