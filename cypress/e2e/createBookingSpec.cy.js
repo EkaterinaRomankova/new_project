5b8375616fcf669bbe98cfcb3c3464e66dc4e6a6
EkaterinaRomankova <catia.romankova@yandex.by>
2023-01-07 19:56:23 +0300

    hell

--- cypress/e2e/createBookingSpec.cy.js
+++ cypress/e2e/createBookingSpec.cy.js
@@ -3,7 +3,6 @@
 import RegisterPage from "../pageObject/RegisterPage"
 import CreateBookingPage from "../pageObject/CreateBookingPage"
 import Header from "../pageObject/header"
-import { each } from "cypress/types/bluebird"
 
 const registerPage = new RegisterPage()
 const createBookingPage = new CreateBookingPage()
@@ -17,21 +16,103 @@ describe('create booking page', () => {
         cy.visit('/')
     })
 
-    it('create booking', function() {
+    xit('verify anavaible dates', function() {
         registerPage.clickOnButtonLogin()
         cy.logIn(this.data.email, this.data.password)
         cy.wait(9000)
+        let d = new Date() 
+        let d1 = d.toLocaleString('en-GB', { timeZone: 'Asia/Bangkok' })
+        let d2 = +d1.slice(0, 2)
         createBookingPage.elements.getAllDatesOfDepTime().then(($el, i) => {
-            let d = new Date() 
-            let d1 = d.toLocaleString('en-GB', { timeZone: 'Asia/Bangkok' })
-            let d2 = +d1.slice(0, 2); 
-            if($el.text().indexOf(d2) == 4){
-                let arr = $el.text().slice(0, $el.text().indexOf(d2))
-                //cy.get('.calendar-wrapper .day-wrapper:nth-child(6)').click()
-                arr.each(el, i).should('')
-                
-
+            if($el.text().indexOf(d2) == -1){
+                createBookingPage.clickOnWeekBack()
             }
-        })       
+        })
+        createBookingPage.elements.getAllDatesOfDepTime().then(($el, i) => {
+            if($el.text().indexOf(d2) == 5){
+                cy.wrap($el.slice(0, $el.text().indexOf(d2))).each(($el => {
+                    cy.wrap($el).should('have.class', this.data.unavaiblaClass)
+                }))
+            } 
+            if($el.text().indexOf(d2) == 4){
+                cy.wrap($el.slice(0, $el.text().indexOf(d2))).each(($el => {
+                    cy.wrap($el).should('have.class', this.data.unavaiblaClass)
+                }))
+            } 
+            if($el.text().indexOf(d2) == 1){
+                cy.wrap($el.slice(0, $el.text().indexOf(d2))).each(($el => {
+                    cy.wrap($el).should('have.class', this.data.unavaiblaClass)
+                }))
+            } 
+            if($el.text().indexOf(d2) == 2){
+                cy.wrap($el.slice(0, $el.text().indexOf(d2))).each(($el => {
+                    cy.wrap($el).should('have.class', this.data.unavaiblaClass)
+                }))
+            } 
+            if($el.text().indexOf(d2) == 3){
+                cy.wrap($el.slice(0, $el.text().indexOf(d2))).each(($el => {
+                    cy.wrap($el).should('have.class', this.data.unavaiblaClass)
+                }))
+            } 
+            if($el.text().indexOf(d2) == 6){
+                cy.wrap($el.slice(0, $el.text().indexOf(d2))).each(($el => {
+                    cy.wrap($el).should('have.class', this.data.unavaiblaClass)
+                }))
+            } 
+        }) 
+    })
+
+    it('vurify that you can not buy ticket for 2 day', function() {
+        registerPage.clickOnButtonLogin()
+        cy.logIn(this.data.email, this.data.password)
+        cy.wait(9000)
+        let d = new Date() 
+        let d1 = d.toLocaleString('en-GB', { timeZone: 'Asia/Bangkok' })
+        let d2 = +d1.slice(0, 2)
+        createBookingPage.elements.getAllDatesOfDepTime().then(($el, i) => {
+            if($el.text().indexOf(d2) == -1){
+                createBookingPage.clickOnWeekBack()
+            }
+        })
+        createBookingPage.elements.getAllDatesOfDepTime().then(($el, i) => {
+            if($el.text().indexOf(d2) == 0){
+                createBookingPage.elements.getBusTickets().should('have.class', this.data.disabledClass)
+            }
+            if($el.text().indexOf(d2) == 5){
+                cy.wait(4000)
+                createBookingPage.elements.getBusTickets().should('have.class', this.data.disabledClass)
+                createBookingPage.clikcOnSunday()
+                cy.wait(4000)
+                createBookingPage.elements.getBusTickets().should('have.class', this.data.disabledClass)
+                createBookingPage.clickOnNextWeek()
+                cy.wait(4000)
+                createBookingPage.elements.getAllDatesOfDepTime().then(() => {
+                    createBookingPage.clickOnMonday()
+                    cy.wait(4000)
+                    createBookingPage.elements.getBusTickets().should('have.class', this.data.disabledClass)
+                    createBookingPage.clickOnTuesday()
+                    cy.wait(4000)
+                    createBookingPage.elements.getBusTickets().should('not.have.class', this.data.disabledClass)
+                })
+            }
+            if($el.text().indexOf(d2) == 6){
+                cy.wait(4000)
+                createBookingPage.elements.getBusTickets().should('have.class', this.data.disabledClass)
+                createBookingPage.clickOnNextWeek()
+                cy.wait(4000)
+                createBookingPage.elements.getAllDatesOfDepTime().then(() => {
+                    createBookingPage.clickOnMonday()
+                    cy.wait(4000)
+                    createBookingPage.elements.getBusTickets().should('have.class', this.data.disabledClass)
+                    createBookingPage.clickOnTuesday()
+                    cy.wait(4000)
+                    createBookingPage.elements.getBusTickets().should('have.class', this.data.disabledClass)
+                    createBookingPage.clickOnWednesday()
+                    cy.wait(4000)
+                    createBookingPage.elements.getBusTickets().should('have.class', this.data.disabledClass) // should not work
+                    //createBookingPage.elements.getBusTickets().should('not.have.class', this.data.disabledClass) //should work
+                })
+            }
+        })        
     })
 })    
--- cypress/pageObject/CreateBookingPage.js
+++ cypress/pageObject/CreateBookingPage.js
@@ -1,7 +1,40 @@
+import { takeWhile } from "cypress/types/lodash"
+
 class CreateBookingPage{
     elements = {
         getTitleOnThePage: () => cy.get('.content-header.booking-header h1'),
-        getAllDatesOfDepTime: () => cy.get('.calendar-wrapper .day-wrapper')
+        getAllDatesOfDepTime: () => cy.get('.calendar-wrapper .day-wrapper'),
+        getBackWeek: () => cy.get('.col-lg-12 .calendar-week-prev'),
+        getBusTickets: () => cy.get('.trips-list-wrapper .vehclass-bus'),
+        getSunday: () => cy.get('.calendar-wrapper .calendar-day-selection-wrapper :nth-child(7)'),
+        getNextWeek: () => cy.get('.col-lg-12 .calendar-week-next'),
+        getMonday: () => cy.get('.calendar-wrapper .calendar-day-selection-wrapper :nth-child(1)'),
+        getTuesday: () => cy.get('.calendar-wrapper .calendar-day-selection-wrapper :nth-child(2)'),
+        getWednesday: () => cy.get('.calendar-wrapper .calendar-day-selection-wrapper :nth-child(3)')
+    }
+
+    clickOnWeekBack(){
+        this.elements.getBackWeek().click()
+    }
+
+    clikcOnSunday(){
+        this.elements.getSunday().click()
+    }
+
+    clickOnNextWeek(){
+        this.elements.getNextWeek().click()
+    }
+
+    clickOnMonday(){
+        this.elements.getMonday().click()
+    }
+
+    clickOnTuesday(){
+        this.elements.getTuesday().click()
+    }
+
+    clickOnWednesday(){
+        takeWhile.elements.getWednesday().click()
     }
 }
 
